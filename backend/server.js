17cbc484b0b67bc798ff3975e59e55b8dfa6a35d
MUNIF <munif.shellbeehaken@gmail.com>
2023-02-20 12:50:00 +0600

    add error middleware.

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
