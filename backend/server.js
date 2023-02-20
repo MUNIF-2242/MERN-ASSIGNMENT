88bb9c0f17eb481871bdadb8176722e6706a26da
MUNIF <munif.shellbeehaken@gmail.com>
2023-02-20 11:50:03 +0600

    create routes complete.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
