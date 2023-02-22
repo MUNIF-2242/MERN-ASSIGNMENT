672aa73f6d28fb7d97c43c546495b3693e114ebd
MUNIF <munif.shellbeehaken@gmail.com>
2023-02-22 10:04:48 +0600

    initial routes and controller setup done for user.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,34 @@
+const asyncHandler = require('express-async-handler');
+
+//@desc Register new user
+//@route POST /api/users
+//@access Public
+
+const registerUser = (req, res) => {
+  //res.status(200).json(goal);
+  res.json({ message: 'Register User' });
+};
+
+//@desc Authenticate a user
+//@route POST /api/users/login
+//@access Public
+
+const loginUser = (req, res) => {
+  //res.status(200).json(goal);
+  res.json({ message: 'Login User' });
+};
+
+//@desc Get user data
+//@route GET /api/users/me
+//@access Public
+
+const getMe = (req, res) => {
+  //res.status(200).json(goal);
+  res.json({ message: 'User data display' });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
